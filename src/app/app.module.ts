9a76fb3edef76d329c9c4258264168deace28640
Edgar <edgarjt97@gmail.com>
2020-05-07 20:33:34 -0500

    install library bootstarp and material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,78 @@
+import {NgModule} from '@angular/core';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatCardModule,
+  MatSelectModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDialogModule,
+  MatSlideToggleModule,
+  MatMenuModule,
+  MatExpansionModule,
+  MatRadioModule,
+  MatTabsModule,
+  MatTreeModule,
+  MatDatepickerModule,
+  MatStepperModule,
+  MatBadgeModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatDatepickerModule,
+    MatStepperModule,
+    MatBadgeModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatDatepickerModule,
+    MatStepperModule,
+    MatBadgeModule
+  ]
+})
+
+export class MaterialModule {}
